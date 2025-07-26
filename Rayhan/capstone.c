915476ad7a0214ceabe41f5c6d1ec2e996ea3cf3
rayhansylvator404 <rayhan21022005@gmail.com>
2025-07-26 12:15:37 +0600

    Add files via upload

---
+++ Rayhan/capstone.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_TRANSACTIONS 100
+typedef struct
+{
+    char type[20];
+    int amount;
+} Transaction;
+
+
+int main(){
+
+}
+void signIn(){
+    
+}
+
