25f4445bc530129501558553b64d12822ed43de4
rayhansylvator404 <rayhan21022005@gmail.com>
2025-07-26 12:36:04 +0600

    Update capstone.c

--- Rayhan/capstone.c
+++ Rayhan/capstone.c
@@ -2,17 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_TRANSACTIONS 100
+
+void homepage();
+
 typedef struct
 {
     char type[20];
     int amount;
 } Transaction;
 
-
-int main(){
-
+int main()
+{
+    homepage();
 }
-void signIn(){
-    
+
+void homePage()
+{
+
+    int option = 0;
+
+    printf("Welcome to Capstone Bank\n");
+    printf("1. Login\n2. Sign In\n");
+    printf("Select an option from this two: ");
+    scanf("%d", &option);
+    switch (option)
+    {
+    case 1:
+        login();
+        break;
+    case 2:
+        signIn();
+        break;
+    default:
+        printf("hoynai abar 1 and 2 er moddhe select korun. Please try again.\n");
+        homePage();
+        break;
+    }
 }
 
